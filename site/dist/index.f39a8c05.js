1852cbd01f56d960d5286238b704c2ebb00d9f3f
tunnelrat16 <tjandmatt@hotmail.com>
2022-05-04 08:58:38 -0700

    Cant get it to work

---
+++ site/dist/index.f39a8c05.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=index.f39a8c05.js.map
---
+++ site/index.ts
@@ -0,0 +1,19 @@
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=91"
+const main = document.querySelector<HTMLBodyElement>("main")
+const pokeBox = document.querySelector<HTMLDivElement>(".pokeBox")
+
+
+type Pokemon = {
+    name: string;
+    sprites: {
+        front_default: string;
+    };
+};
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+.then((response) => response.json())
+.then((pikachu: Pokemon) => {
+    pokeBox.innerHTML = `
+    <img src="${pikachu.sprites.front_default}" alt="${pikachu.name}" />
+    `;
+});
